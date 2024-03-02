d7ecc105cec06ffc5457c756d9bf61599f835e5d
Arnab Mondal <arnabjuniormondal123@gmail.com>
2024-03-03 00:46:49 +0530

    feat: added navbar and changed header section image

--- src/app/components/HeaderSection.tsx
+++ src/app/components/HeaderSection.tsx
@@ -17,7 +17,7 @@ export default function HeaderSection() {
                         <TypeAnimation
                             className=""
                             sequence={["Arnab Mondal", 1000, "Developer", 1000]}
-                            wrapper="span"
+                            wrapper="p"
                             speed={50}
                             repeat={Infinity}
                         />
@@ -27,19 +27,19 @@ export default function HeaderSection() {
                         elit. Non dolore, fugiat magnam. In, architecto ipsum.
                     </p>
                     <div>
-                        <button className="px-6 py-3 mx-3 my-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% hover:bg-cyan-900 font-semibold">
+                        <button className="px-6 py-3 lg:mr-3 lg:my-1 mb-2 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% hover:bg-cyan-900 font-semibold">
                             Hire Me
                         </button>
-                        <button className="px-6 py-3 mx-3 my-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-l from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  hover:bg-cyan-900 font-semibold">
+                        <button className="px-6 py-3 lg:mx-3 lg:my-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-l from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  hover:bg-cyan-900 font-semibold">
                             Download CV
                         </button>
                     </div>
                 </div>
                 <div className="col-span-5 place-self-center mt-5 lg:mt-0">
-                    <div className="rounded-full bg-[#181818] w-[300px] h-[250px] lg:w-[400px] lg:h-[370px] relative">
+                    <div className="rounded-full bg-[#181818] w-[300px] h-[270px] lg:w-[400px] lg:h-[370px] relative">
                         <Image
-                            src="/images/homeImage.png"
-                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[300px] h-[200px] lg:w-[400px] lg:h-[300px]"
+                            src="/images/homeImage2.png"
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[300px] h-[250px] lg:w-[350px] lg:h-[300px]"
                             alt="image"
                             width={500}
                             height={500}
